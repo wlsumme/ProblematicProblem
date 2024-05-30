8f44ff976ae58f45bf6e64cf6fc8aacdb4860ef0
William Summe <wlsumme@gmail.com>
2024-05-30 14:45:18 -0500

    done

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -19,113 +19,125 @@ namespace ProblematicProblem
             bool cont;
             var contResponse = Console.ReadLine().ToLower();
 
-            
 
 
 
-                if (contResponse == "yes")
+
+            if (contResponse == "yes")
+            {
+                cont = true;
+            }
+            else
+            {
+                cont = false;
+            }
+            Console.WriteLine();
+            Console.Write("We are going to need your information first! What is your name? ");
+            string userName = Console.ReadLine();
+
+
+            Console.WriteLine();
+            Console.Write("What is your age? ");
+            int userAge = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine();
+
+
+            Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
+            bool seeList = (Console.ReadLine().ToLower() == "sure") ? true : false;
+
+            if (seeList)
+            {
+                foreach (string activity in activities)
                 {
-                    cont = true;
+                    Console.Write($"{activity} ");
+                    Thread.Sleep(250);
                 }
-                else
+                Console.WriteLine();
+                Console.Write("Would you like to add any activities before we generate one? yes/no: ");
+                bool addToList = (Console.ReadLine().ToLower() == "yes") ? true : false;
+
+
+
+                Console.WriteLine();
+
+
+                while (addToList)
                 {
-                    cont = false;
-                }
-                Console.WriteLine();
-                Console.Write("We are going to need your information first! What is your name? ");
-                string userName = Console.ReadLine();
-               
-                
-                Console.WriteLine();
-                Console.Write("What is your age? ");
-                int userAge = int.Parse(Console.ReadLine());
-               
-                
-                Console.WriteLine();
-                
-                
-                Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-                bool seeList = (Console.ReadLine().ToLower() == "sure") ? true : false;
-               
-                if (seeList)
-                {
+                    Console.Write("What would you like to add? ");
+                    string userAddition = Console.ReadLine();
+                    activities.Add(userAddition);
+
                     foreach (string activity in activities)
                     {
                         Console.Write($"{activity} ");
                         Thread.Sleep(250);
                     }
-                    Console.WriteLine();
-                    Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-                    bool addToList = (Console.ReadLine().ToLower() == "yes") ? true : false;
-                   
-                    
-                    
-                    Console.WriteLine();
-                   
-                    
-                    while (addToList)
-                    {
-                        Console.Write("What would you like to add? ");
-                        string userAddition = Console.ReadLine();
-                        activities.Add(userAddition);
-                       
-                        foreach (string activity in activities)
-                        {
-                            Console.Write($"{activity} ");
-                            Thread.Sleep(250);
-                        }
-                        
-                        Console.WriteLine();
-                        Console.WriteLine("Would you like to add more? yes/no: ");
-                        addToList = (Console.ReadLine().ToLower() == "yes") ?true : false;
-                        Console.WriteLine();
-                    }
-                }
 
-                while (cont)
+                    Console.WriteLine();
+                    Console.WriteLine("Would you like to add more? yes/no: ");
+                    addToList = (Console.ReadLine().ToLower() == "yes") ? true : false;
+                    Console.WriteLine();
+                }
+            }
+
+            while (cont)
+            {
+                Console.Write("Connecting to the database");
+
+                for (int i = 0; i < 10; i++)
                 {
-                    Console.Write("Connecting to the database");
-                   
-                    for (int i = 0; i < 10; i++)
-                    {
-                        Console.Write(". ");
-                        Thread.Sleep(500);
-                    }
-                    Console.WriteLine();
-                    Console.Write("Choosing your random activity");
-                    
-                    for (int i = 0; i < 9; i++)
-                    {
-                        Console.Write(". ");
-                        Thread.Sleep(500);
-                    }
-                   
-                    Console.WriteLine();
-                    int randomNumber = rng.Next(activities.Count);
-                    string randomActivity = activities[randomNumber];
-                    
-                    
-                    if (userAge < 21 && randomActivity == "Wine Tasting")
-                    {
-                        Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
-                        Console.WriteLine("Pick something else!");
-                        activities.Remove(randomActivity);
-                         randomNumber = rng.Next(activities.Count);
-                         randomActivity = activities[randomNumber];
-                    }
+                    Console.Write(". ");
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine();
+                Console.Write("Choosing your random activity");
 
-                
-                    Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
-                    Console.WriteLine();
-                    contResponse = Console.ReadLine().ToLower(); 
-                if (contResponse != "redo")
+                for (int i = 0; i < 9; i++)
                 {
-                    cont = false;
-                }
+                    Console.Write(". ");
+                    Thread.Sleep(500);
                 }
 
-           
+                Console.WriteLine();
+                int randomNumber = rng.Next(activities.Count);
+                string randomActivity = activities[randomNumber];
 
+
+                if (userAge < 21 && randomActivity == "Wine Tasting")
+                {
+                    Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
+                    Console.WriteLine("Pick something else!");
+                    activities.Remove(randomActivity);
+                    randomNumber = rng.Next(activities.Count);
+                    randomActivity = activities[randomNumber];
+                }
+
+
+                Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.WriteLine();
+                var userResponse = Console.ReadLine().ToLower();
+                while (userResponse == "keep" && userResponse != "redo")
+                {
+                    Console.WriteLine("Invalid response");
+                    userResponse = Console.ReadLine().ToLower();
+
+                }
+                if (userResponse == "keep")
+                {
+                    Console.WriteLine($"Ok I hope you enjoy {randomActivity}. Have a great day {userName}!");
+                }
+                else
+                {
+                    cont = true;
+                }
+
+
+
+
+
+            }
         }
     }
 }
